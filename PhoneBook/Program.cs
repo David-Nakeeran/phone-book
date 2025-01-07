a4f939c35cac5b5a50bcc7b3b46b5bf6ee922930
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-07 16:57:58 +0000

    Added Configuration builder

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+
+namespace PhoneBook;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Loads settings from JSON file and builds configuration object
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        // Create service collection
+        var services = new ServiceCollection();
+
+        // Register the services
+        services.
+
+
+    }
+}
+
