fdb0efd6682df20ec911422fff1eea97c897f4a2
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-07 16:56:53 +0000

    Added ContactDetail model

---
+++ PhoneBook/Models/ContactDetail.cs
@@ -0,0 +1,11 @@
+
+
+namespace PhoneBook.Models;
+
+class ContactDetail
+{
+    int Id { get; set; }
+    string? Name { get; set; }
+    string? Email { get; set; }
+    string? PhoneNumber { get; set; }
+}
