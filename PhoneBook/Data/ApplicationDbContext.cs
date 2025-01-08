aff2075f5f2e38471a1eda56d96e2f04877dc955
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-08 17:11:05 +0000

    Added comments

--- PhoneBook/Data/ApplicationDbContext.cs
+++ PhoneBook/Data/ApplicationDbContext.cs
@@ -11,4 +11,11 @@ class ApplicationDbContext : DbContext
     {
 
     }
+    // ApplicationDbContext is used by Entity Framework Core to interact with the database.
+    // A DbSet property representing the ContactDetails table in the database.
+    // Entity Framework will use this property to query and save instances of ContactDetail.
+    // The constructor takes DbContextOptions, which provide configuration settings for EF Core,
+    // such as the database provider and connection details. These options are passed to the base DbContext class.
+    // Passes the options to the base DbContext constructor so EF Core can use the configuration.
+
 }
