02049817d5bb5684c41b17097a5b04a073e5a253
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-11 13:46:27 +0000

    Added start method - displays menu to user while isAppActive is true - uses switch statement to handle users input

--- PhoneBook/Coordinators/AppCoordinator.cs
+++ PhoneBook/Coordinators/AppCoordinator.cs
@@ -1,14 +1,42 @@
-
+using PhoneBook.Enums;
+using PhoneBook.Views;
 
 namespace PhoneBook.Coordinators;
 
 class AppCoordinator
 {
+    private readonly MenuHandler _menuHandler;
 
-
-
+    public AppCoordinator(MenuHandler menuHandler)
+    {
+        _menuHandler = menuHandler;
+    }
     internal void Start()
     {
-        Console.WriteLine("Application has started");
+        bool isAppActive = true;
+
+        while (isAppActive)
+        {
+            var userSelection = _menuHandler.ShowMenu();
+            switch (userSelection)
+            {
+                case MenuOptions.ViewAllRecords:
+                    Console.WriteLine("View all records");
+                    break;
+                case MenuOptions.InsertRecord:
+                    Console.WriteLine("Insert Record");
+                    break;
+                case MenuOptions.UpdateRecord:
+                    Console.WriteLine("Update Record");
+                    break;
+                case MenuOptions.DeleteRecord:
+                    Console.WriteLine("Delete record");
+                    break;
+                case MenuOptions.Quit:
+                    Console.WriteLine("Quit app");
+                    isAppActive = false;
+                    break;
+            }
+        }
     }
 }
