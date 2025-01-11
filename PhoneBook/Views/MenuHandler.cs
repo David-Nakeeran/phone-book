a9962ba9e039f03f86d6a0037b2a69a0621a0a2d
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-11 13:45:32 +0000

    Created showmenu method - uses spectre to display and prompt user

--- PhoneBook/Views/MenuHandler.cs
+++ PhoneBook/Views/MenuHandler.cs
@@ -1,13 +1,26 @@
 using Spectre.Console;
-
+using PhoneBook.Enums;
 namespace PhoneBook.Views;
 
 class MenuHandler
 {
-
-
-    internal string? ShowMenu()
+    internal MenuOptions ShowMenu()
     {
-        return "return menu";
+        Console.Clear();
+        AnsiConsole.MarkupLine("[bold]Main Menu[/]");
+        AnsiConsole.WriteLine();
+        AnsiConsole.MarkupLine("[green](Use arrow keys to navigate, then press enter)[/]");
+        AnsiConsole.WriteLine();
+        var userSelection = AnsiConsole.Prompt(
+            new SelectionPrompt<MenuOptions>()
+                .Title("Select an option")
+                .AddChoices(
+                    MenuOptions.ViewAllRecords,
+                    MenuOptions.InsertRecord,
+                    MenuOptions.UpdateRecord,
+                    MenuOptions.DeleteRecord,
+                    MenuOptions.Quit
+                    ));
+        return userSelection;
     }
 }
