d5e550e51cc10b6a70ff63bc2f0f6e408bea683a
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-12 16:08:53 +0000

    Constructing method to get user input

--- PhoneBook/Controllers/ContactController.cs
+++ PhoneBook/Controllers/ContactController.cs
@@ -1,6 +1,22 @@
+using PhoneBook.Utilities;
+using Spectre.Console;
+
+
 namespace PhoneBook.Controllers;
 
 class ContactController
 {
+    private readonly Validation _validation;
 
+    public ContactController(Validation validation)
+    {
+        _validation = validation;
+    }
+
+    internal string? GetContactName()
+    {
+        var contactName = AnsiConsole.Ask<string>("Please enter name of contact or enter 0 to return to main menu");
+        contactName = _validation.CheckInputNullOrWhitespace(string message, string ? input)
+        // then return string
+    }
 }
