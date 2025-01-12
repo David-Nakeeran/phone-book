73f8ab5aa331d1ace30f11ac6d306bf1f27cd21a
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-12 16:09:09 +0000

    Added Validation methods for strings

--- PhoneBook/Utilities/Validation.cs
+++ PhoneBook/Utilities/Validation.cs
@@ -1,6 +1,33 @@
+using Spectre.Console;
+
 namespace PhoneBook.Utilities;
 
 class Validation
 {
+    internal string CheckInputNullOrWhitespace(string message, string? input)
+    {
+        while (string.IsNullOrWhiteSpace(input))
+        {
+            input = AnsiConsole.Ask<string>(message);
+        }
+        return input;
+    }
 
+    internal bool IsCharZero(string input)
+    {
+        if (input.Length == 1 && input.Equals('0')) return true;
+        return false;
+    }
+
+    internal bool IsCharValid(string input)
+    {
+        return input.All(Char.IsLetter);
+    }
+
+    internal string? ValidateString(string message, string? input)
+    {
+        input = CheckInputNullOrWhitespace(message, input);
+        if (IsCharZero(input)) return input;
+
+    }
 }
