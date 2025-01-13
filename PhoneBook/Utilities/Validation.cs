ebf38e6558023cbf392546c4c846f22b7319662b
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-13 16:26:56 +0000

    Added email validation and Ilogger

--- PhoneBook/Utilities/Validation.cs
+++ PhoneBook/Utilities/Validation.cs
@@ -1,9 +1,17 @@
+using Microsoft.Extensions.Logging;
 using Spectre.Console;
+using System.Net.Mail;
 
 namespace PhoneBook.Utilities;
 
 class Validation
 {
+    private readonly ILogger<Validation> _logger;
+
+    public Validation(ILogger<Validation> logger)
+    {
+        _logger = logger;
+    }
     internal string CheckInputNullOrWhitespace(string message, string? input)
     {
         while (string.IsNullOrWhiteSpace(input))
@@ -15,7 +23,7 @@ class Validation
 
     internal bool IsCharZero(string input)
     {
-        if (input.Length == 1 && input.Equals('0')) return true;
+        if (input.Length == 1 && input.Equals("0")) return true;
         return false;
     }
 
@@ -28,6 +36,7 @@ class Validation
     {
         input = CheckInputNullOrWhitespace(message, input);
         if (IsCharZero(input)) return input;
+        if (IsCharValid(input)) return input;
         while (!IsCharValid(input))
         {
             AnsiConsole.WriteLine(invalidMessage);
@@ -37,5 +46,35 @@ class Validation
             if (IsCharZero(input)) return input;
         }
         return input;
+    }
+
+    internal bool IsEmailValid(string emailAddress)
+    {
+        try
+        {
+            var email = new MailAddress(emailAddress);
+            return true;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error occurred while validating email address");
+            return false;
+        }
+    }
+
+    internal string? ValidateEmailAddress(string message, string invalidMessage, string emailAddress)
+    {
+        emailAddress = CheckInputNullOrWhitespace(message, emailAddress);
+        if (IsCharZero(emailAddress)) return emailAddress;
+        if (IsEmailValid(emailAddress)) return emailAddress;
+        while (!IsEmailValid(emailAddress))
+        {
+            AnsiConsole.WriteLine(invalidMessage);
+            Console.ReadKey(true);
+            emailAddress = AnsiConsole.Ask<string>(message);
+            emailAddress = CheckInputNullOrWhitespace(message, emailAddress);
+            if (IsCharZero(emailAddress)) return emailAddress;
+        }
+        return emailAddress;
     }
 }
