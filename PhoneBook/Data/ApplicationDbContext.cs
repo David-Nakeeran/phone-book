3d12d552374d9754d997dec57cba6defeceb8ef5
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-15 16:32:50 +0000

    Formatting

--- PhoneBook/Data/ApplicationDbContext.cs
+++ PhoneBook/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ namespace PhoneBook.Data;
 
 class ApplicationDbContext : DbContext
 {
-    public DbSet<ContactDetail>? ContactDetails { get; set; }
+    public DbSet<ContactDetail> ContactDetails { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
