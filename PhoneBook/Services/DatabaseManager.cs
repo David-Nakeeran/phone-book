15edeef5cf9d0dd68077d5dee56e44b3f60830fa
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-15 16:42:18 +0000

    Added CreateNewContact and GetContactList methods

---
+++ PhoneBook/Services/DatabaseManager.cs
@@ -0,0 +1,30 @@
+using PhoneBook.Models;
+using PhoneBook.Data;
+
+namespace PhoneBook.Services;
+
+class DatabaseManager
+{
+    private readonly ApplicationDbContext _applicationDbContext;
+
+    public DatabaseManager(ApplicationDbContext applicationDbContext)
+    {
+        _applicationDbContext = applicationDbContext;
+    }
+
+    internal void CreateNewContact(string? name, string? email, string? phoneNumber)
+    {
+        _applicationDbContext.Add(new ContactDetail
+        {
+            Name = name,
+            Email = email,
+            PhoneNumber = phoneNumber
+        });
+        _applicationDbContext.SaveChanges();
+    }
+
+    internal List<ContactDetail> GetContactList()
+    {
+        return _applicationDbContext.ContactDetails.ToList();
+    }
+}
