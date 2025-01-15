b696b428877b90f1a68971ada3f3f6fda69404b2
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-15 16:42:07 +0000

    Added ContactDetailDTO

---
+++ PhoneBook/Models/ContactDetailDTO.cs
@@ -0,0 +1,11 @@
+
+
+namespace PhoneBook.Models;
+
+public class ContactDetailDTO
+{
+    public int DisplayId { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+}
