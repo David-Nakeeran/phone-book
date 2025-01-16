5cc9962465b9bfaecc899187e61b3d5d21c62eeb
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-16 16:34:45 +0000

    Added two methods IsListEmpty and PrintContacts

---
+++ PhoneBook/Utilities/ListManager.cs
@@ -0,0 +1,36 @@
+using PhoneBook.Models;
+using Spectre.Console;
+
+namespace PhoneBook.Utilities;
+
+class ListManager
+{
+    internal bool IsListEmpty<T>(List<T> list) => list.Any();
+
+    internal void PrintContacts(List<ContactDetailDTO> list)
+    {
+        var table = new Table();
+
+        table.Title($"Contacts");
+
+        table.AddColumn("ID");
+        table.AddColumn("Name");
+        table.AddColumn("Email");
+        table.AddColumn("Mobile Number");
+
+        foreach (var contact in list)
+        {
+
+            table.AddRow(
+            contact.DisplayId.ToString(),
+            contact.Name.ToString(),
+            contact.Email.ToString(),
+            contact.PhoneNumber.ToString()
+            );
+        }
+
+        AnsiConsole.Write(table);
+    }
+
+
+}
