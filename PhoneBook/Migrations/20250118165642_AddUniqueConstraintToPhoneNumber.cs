b199e4e944542cc34358c279969301770208b735
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-18 17:05:57 +0000

    Updated migrations

---
+++ PhoneBook/Migrations/20250118165642_AddUniqueConstraintToPhoneNumber.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhoneBook.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueConstraintToPhoneNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "ContactDetails",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactDetails_PhoneNumber",
+                table: "ContactDetails",
+                column: "PhoneNumber",
+                unique: true,
+                filter: "[PhoneNumber] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ContactDetails_PhoneNumber",
+                table: "ContactDetails");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "ContactDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+    }
+}
