f42128c3b6f5c61711b9e8db2d63fc872579d548
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-22 17:00:31 +0000

    Added Email detail model

---
+++ PhoneBook/Models/EmailDetail.cs
@@ -0,0 +1,12 @@
+
+
+namespace PhoneBook.Models;
+
+public class EmailDetail
+{
+    public string? RecipientName { get; set; }
+    public string? RecipientEmail { get; set; }
+    public string? Subject { get; set; }
+    public string? Body { get; set; }
+}
+
