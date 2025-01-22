6618bd634d3cd58b29890256bc79407ee277cee3
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-22 17:04:10 +0000

    Added MailSettings class

--- PhoneBook/Models/MailSettings.cs
+++ PhoneBook/Models/MailSettings.cs
@@ -10,6 +10,6 @@ public class MailSettings
     public string? EmailId { get; set; }
     public string? UserName { get; set; }
     public string? Password { get; set; }
-    public bool UseSSL { get; set; }
+    public bool UseStartTls { get; set; }
 }
 
