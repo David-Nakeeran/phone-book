d702e771fc5f76aa6800da90fcbc90e77d8a26c6
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-24 16:52:07 +0000

    Added Category model

---
+++ PhoneBook/Models/Category.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PhoneBook.Models;
+
+public class Category
+{
+    [Key]
+    public int CategoryId { get; set; }
+
+    [Required]
+    public required string CategoryName { get; set; }
+
+    public List<ContactDetail>? Contacts { get; set; }
+    // navigation property is a collection of all ContactDetail records that have the matching CategoryId
+}
