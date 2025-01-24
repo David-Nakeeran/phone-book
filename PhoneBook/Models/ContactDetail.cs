3d27f2b3a43f5ce7a2edcd703a951878f2426021
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-24 16:53:42 +0000

    Added data notation

--- PhoneBook/Models/ContactDetail.cs
+++ PhoneBook/Models/ContactDetail.cs
@@ -1,11 +1,28 @@
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace PhoneBook.Models;
 
+[Index(nameof(PhoneNumber), IsUnique = true)]
 public class ContactDetail
 {
+    [Key]
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Email { get; set; }
-    public string? PhoneNumber { get; set; }
+
+    [Required]
+    public required string Name { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public required string Email { get; set; }
+
+    [Required]
+    [Phone]
+    public required string PhoneNumber { get; set; }
+
+    public int CategoryId { get; set; }
+
+    [ForeignKey(nameof(CategoryId))]
+    public Category? Category { get; set; }
 }
