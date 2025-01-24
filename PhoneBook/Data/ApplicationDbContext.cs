82edca538e25295e66937d17e661a6996038a89b
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-24 16:54:17 +0000

    On model created, seeded data

--- PhoneBook/Data/ApplicationDbContext.cs
+++ PhoneBook/Data/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ namespace PhoneBook.Data;
 class ApplicationDbContext : DbContext
 {
     public DbSet<ContactDetail> ContactDetails { get; set; }
+    public DbSet<Category> Categories { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
@@ -14,9 +15,12 @@ class ApplicationDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<ContactDetail>()
-            .HasIndex(c => c.PhoneNumber)
-            .IsUnique();
+        modelBuilder.Entity<Category>().HasData(
+            new Category { CategoryId = 1, CategoryName = "Friends" },
+            new Category { CategoryId = 2, CategoryName = "Family" },
+            new Category { CategoryId = 3, CategoryName = "Work" },
+            new Category { CategoryId = 4, CategoryName = "Other" }
+        );
     }
     // ApplicationDbContext is used by Entity Framework Core to interact with the database.
     // A DbSet property representing the ContactDetails table in the database.
