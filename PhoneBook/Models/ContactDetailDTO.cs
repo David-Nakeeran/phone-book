876c8d18293bd3fee72cb578a1c914e30da2c5e4
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-01-25 16:31:10 +0000

    Added CategoryName property

--- PhoneBook/Models/ContactDetailDTO.cs
+++ PhoneBook/Models/ContactDetailDTO.cs
@@ -8,4 +8,5 @@ public class ContactDetailDTO
     public required string Name { get; set; }
     public required string Email { get; set; }
     public required string PhoneNumber { get; set; }
+    public required string CategoryName { get; set; }
 }
