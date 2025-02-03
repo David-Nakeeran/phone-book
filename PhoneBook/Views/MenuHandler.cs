9c178392d2454863d49038f448e92f4ff1b75c6d
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-03 13:18:50 +0000

    Choices now use get display name method

--- PhoneBook/Views/MenuHandler.cs
+++ PhoneBook/Views/MenuHandler.cs
@@ -1,26 +1,34 @@
 using Spectre.Console;
 using PhoneBook.Enums;
+using PhoneBook.Utilities;
 namespace PhoneBook.Views;
 
 class MenuHandler
 {
-    internal MenuOptions ShowMenu()
+    private readonly EnumHelper _enumHelper;
+
+    public MenuHandler(EnumHelper enumHelper)
     {
-        // Console.Clear();
+        _enumHelper = enumHelper;
+    }
+
+    internal string ShowMenu()
+    {
+        Console.Clear();
         AnsiConsole.MarkupLine("[bold]Main Menu[/]");
         AnsiConsole.WriteLine();
         AnsiConsole.MarkupLine("[green](Use arrow keys to navigate, then press enter)[/]");
         AnsiConsole.WriteLine();
         var userSelection = AnsiConsole.Prompt(
-            new SelectionPrompt<MenuOptions>()
+            new SelectionPrompt<string>()
                 .Title("Select an option")
                 .AddChoices(
-                    MenuOptions.ViewAllRecords,
-                    MenuOptions.InsertRecord,
-                    MenuOptions.UpdateRecord,
-                    MenuOptions.DeleteRecord,
-                    MenuOptions.SendEmail,
-                    MenuOptions.Quit
+                    _enumHelper.GetDisplayName(MenuOptions.ViewAllRecords),
+                    _enumHelper.GetDisplayName(MenuOptions.InsertRecord),
+                    _enumHelper.GetDisplayName(MenuOptions.UpdateRecord),
+                    _enumHelper.GetDisplayName(MenuOptions.DeleteRecord),
+                    _enumHelper.GetDisplayName(MenuOptions.SendEmail),
+                    _enumHelper.GetDisplayName(MenuOptions.Quit)
                     ));
         return userSelection;
     }
