17b4e61519a7dd74c615dfc7e77d02834c561bc1
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-03 13:19:43 +0000

    Added Display attribute

--- PhoneBook/Enums/MenuOptions.cs
+++ PhoneBook/Enums/MenuOptions.cs
@@ -1,11 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PhoneBook.Enums;
 
 internal enum MenuOptions
 {
+    [Display(Name = "View All Records")]
     ViewAllRecords,
+
+    [Display(Name = "Insert Record")]
     InsertRecord,
+
+    [Display(Name = "Update Record")]
     UpdateRecord,
+
+    [Display(Name = "Delete Record")]
     DeleteRecord,
+
+    [Display(Name = "Send Email")]
     SendEmail,
+
+    [Display(Name = "Quit")]
     Quit
 }
